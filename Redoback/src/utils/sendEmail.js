73899eaa017d7141a97082f7e6fdf0e89f29d7ef
ris1005 <yrishikareddy123@gmail.com>
2026-05-27 15:45:35 +0530

    Nodemailer change

--- Redoback/src/utils/sendEmail.js
+++ Redoback/src/utils/sendEmail.js
@@ -1,16 +1,22 @@
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-  logger:true,
-  debug:true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+});
+
+// Verify SMTP connection
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log("VERIFY ERROR:", error);
+  } else {
+    console.log("Server is ready to send emails");
+  }
 });
 
 const sendEmail = async (to, subject, text) => {
@@ -21,6 +27,5 @@ const sendEmail = async (to, subject, text) => {
     text,
   });
 };
-
 
 module.exports = sendEmail;
