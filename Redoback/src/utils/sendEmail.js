8da8ff2d9ac184a09d66ccbb4394ce1e3864982a
ris1005 <yrishikareddy123@gmail.com>
2026-05-27 16:37:32 +0530

    brevo added

--- Redoback/src/utils/sendEmail.js
+++ Redoback/src/utils/sendEmail.js
@@ -1,22 +1,28 @@
 
-const { Resend } = require("resend");
+const nodemailer = require("nodemailer");
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  auth: {
+    user: process.env.BREVO_USER,
+    pass: process.env.BREVO_PASS,
+  },
+});
 
 const sendEmail = async (to, subject, text) => {
   try {
-    const data = await resend.emails.send({
-      from: "onboarding@resend.dev",
+    const info = await transporter.sendMail({
+      from: process.env.BREVO_USER,
       to,
       subject,
       text,
     });
 
-    console.log("Email sent:", data);
-  } catch (error) {
-    console.error(error);
+    console.log("Mail sent:", info.response);
+  } catch (err) {
+    console.error(err);
   }
 };
 
 module.exports = sendEmail;
-
