b8717106e583ae0a89271f725cebb580690e376b
ris1005 <yrishikareddy123@gmail.com>
2026-05-29 15:26:14 +0530

    backend to vercel

---
+++ Redoback/api/index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const app = require("../src/app");
+const connectDB = require("../src/config/db");
+
+// Connect to MongoDB
+connectDB();
+
+module.exports = app;
--- Redoback/src/config/db.js
+++ Redoback/src/config/db.js
@@ -1,13 +1,28 @@
 const mongoose = require("mongoose");
-console.log(process.env.MONGO_URI);
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
-  } catch (err) {
-    console.error("❌ DB error", err);
-    process.exit(1);
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  };
+}
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
   }
-};
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  console.log("✅ MongoDB connected");
+  return cached.conn;
+}
 
 module.exports = connectDB;
